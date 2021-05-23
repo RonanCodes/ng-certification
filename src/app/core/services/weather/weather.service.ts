499922138b86c9d6ae2827af8d24f7eafbcdb8b8
Ronan Connolly <ronanc314@gmail.com>
2021-05-23 17:41:20 +0100

    Create skeleton for the weather service

--- src/app/core/services/weather/weather.service.ts
+++ src/app/core/services/weather/weather.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getWeatherByZipCode(zipCode: number): Observable<Weather> {
+    return of();
+  }
+
+  /**
+   * The OpenWeather API doesn't seem to allow for multi zip code in one request.
+   * So we will do a ForkJoin to do simultaneous requests.
+   */
+  getWeatherByZipCodes(zipCodes: number[]): Observable<Weather> {
+    // Use a ForkJoin:
+    return of();
+  }
+
+  getFiveDayForecastByZipCode(): Observable<Weather> {
+    return of();
+  }
 }

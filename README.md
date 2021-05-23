bec8b6e27fe57f068dde518b76ebf99095988d12
Ronan Connolly <ronanc314@gmail.com>
2021-05-23 15:55:35 +0100

    Update project dependencies to latest for a new Angular v11 project, and get all the scripts working. Fix TSlint errors, and tidy up default template.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # angular-8t3kmd
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8t3kmd)
+
+## Note on the project update
+This project has been updated to the latest packages in line with NG version 11.
+
+These libs were not updated:
+- "bootstrap"
+- "core-js"
+- "popper.js"
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
-import './polyfills';
+// import './polyfills';
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+/**
+ * Window with the NgModuleRef ref value.
+ *
+ * Keeps the TS compiler happy.
+ */
+class NgWindow extends Window {
+  public ngRef?: NgModuleRef<AppModule> | undefined;
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+  // Ensure Angular destroys itself on hot reloads:
+  const ngWindow: NgWindow = window;
+
+  if (ngWindow['ngRef']) {
+    (ngWindow['ngRef']).destroy();
   }
-  window['ngRef'] = ref;
+  ngWindow['ngRef'] = ref;
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));

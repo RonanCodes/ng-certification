56a76a5abbf0dba22b391706ddeaa81d7dc03fe1
Ronan Connolly <ronanc314@gmail.com>
2021-05-23 16:12:46 +0100

    Move template code into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { ZipcodeInputComponent } from './zipcode-input/zipcode-input.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, DashboardComponent, ForecastComponent, WeatherCardComponent, ZipcodeInputComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
